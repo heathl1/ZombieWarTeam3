3fd8141a3030a2f2bf360ef765e4637d56c25e6c
AsiaElmi <elmia7@csp.edu>
2025-08-17 12:48:56 +0500

    Add weapon name methods and improve attack damage calculation

--- src/Unit.java
+++ src/Unit.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 public class Unit {
     private int health;
     private int maxHealth;
@@ -9,7 +8,7 @@ public class Unit {
     private String unitType;
     private int id; // id to better identify which character is performing which action
     private Weapon weapon;
-
+    
     public Unit(int health, int maxHealth, int attackPower, boolean alive, boolean isZombie, String unitType, int id) {
         this.health = health;
         this.maxHealth = maxHealth;
@@ -19,30 +18,31 @@ public class Unit {
         this.unitType = unitType;
         this.id = id;
     }
-
+    
     public void attack(Unit target) {
         target.takeDamage(this.attackPower);
     }
-
+    
     public void takeDamage(int damage) {
         this.health = this.health - damage;
         if (this.health <= 0) {
             this.alive = false;
         }
     }
-
+    
     public boolean isAlive() {
         return this.alive;
     }
-
+    
     public int getHealth() {
         return this.health;
     }
-
+    
     public int getAttackPower() {
         if(!this.unitType.equals("Tank")  && !this.unitType.equals("Common Infected")) {
             if(this.weapon == null) {
-                System.out.println(this.unitType + this.id + "has npo weapo");
+                System.out.println(this.unitType + this.id + " has no weapon");
+                return this.attackPower; // Use base attack power if no weapon
             }
             Random random = new Random();
             Integer aim = random.nextInt(99) + 1;
@@ -53,23 +53,48 @@ public class Unit {
         }
         return attackPower;
     }
-
+    
+    // New method to get attack power with weapon (for better control in battle)
+    public int getAttackPowerWithWeapon() {
+        if(!this.unitType.equals("Tank")  && !this.unitType.equals("Common Infected")) {
+            if(this.weapon == null) {
+                return this.attackPower; // Use base attack power if no weapon
+            }
+            Random random = new Random();
+            Integer aim = random.nextInt(100) + 1; // 1-100 for percentage
+            if(aim > this.weapon.getAccuracy()) {
+                return 0; // Miss
+            }
+            return this.weapon.getDamage();
+        }
+        return attackPower;
+    }
+    
+    // New method to get weapon name for kill messages
+    public String getWeaponName() {
+        if (this.weapon == null) {
+            return "bare hands"; // Fallback if no weapon
+        }
+        return this.weapon.getName();
+    }
+    
     public String getUnitType() {
         return this.unitType;
     }
-
+    
     public boolean isZombie() {
         return this.isZombie;
     }
-
-    public int getId() { return this.id; }
-
+    
+    public int getId() { 
+        return this.id; 
+    }
+    
     public void setWeapon(Weapon weapon) {
         this.weapon = weapon;
     }
-
+    
     public String toString() {
         return String.format("%s %d", unitType, getId());
     }
-
 }
