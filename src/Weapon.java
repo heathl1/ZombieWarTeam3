2b46447a3001b54ee5ae7bfa75d2d61af890f2d5
AsiaElmi <elmia7@csp.edu>
2025-08-17 12:49:59 +0500

    Fix combat system: prevent duplicate kills, show weapon names

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,17 +1,24 @@
+
 public class Weapon {
     private Integer damage;
     private Integer accuracy;
-
-    public Weapon(Integer damage, Integer accuracy) {
+    private String name;
+    
+    public Weapon(Integer damage, Integer accuracy, String name) {
         this.damage = damage;
         this.accuracy = accuracy;
+        this.name = name;
     }
-
+    
     public Integer getDamage() {
         return this.damage;
     }
-
+    
     public Integer getAccuracy() {
         return this.accuracy;
+    }
+    
+    public String getName() {
+        return this.name;
     }
 }
